5064387a5df557441de300e0666b643bb4c99492
victor <lncwwn@gmail.com>
2015-06-16 17:12:32 +0800

    refined

--- fragment_hub/urls.py
+++ fragment_hub/urls.py
@@ -8,4 +8,9 @@ urlpatterns = [
 
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$', 'fragment_hub.views.index'),
+
+    # for user signup and login
+    url(r'^user/login/', 'fragment_hub.views.login'),
+    url(r'^user/signup/', 'fragment_hub.views.signup'),
+    url(r'^user/change_password/', 'fragment_hub.views.change_password'),
 ]
--- fragment_hub/views.py
+++ fragment_hub/views.py
@@ -2,3 +2,12 @@ from django.shortcuts import render_to_response
 
 def index(request):
     return render_to_response('index.html')
+
+def login(request):
+    return render_to_response('user/login.html')
+
+def signup(request):
+    return render_to_response('user/signup.html')
+
+def change_password(request):
+    return render_to_response('user/change_password.html')
