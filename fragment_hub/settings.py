1401dbcad6b778c675741813b50da85cf6106db8
victor <lncwwn@gmail.com>
2015-06-16 15:13:51 +0800

    refined

--- fragment_hub/settings.py
+++ fragment_hub/settings.py
@@ -76,8 +76,14 @@ WSGI_APPLICATION = 'fragment_hub.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': 'db_fragment_hub',
+        'USER': 'root',
+        'PASSWORD': 'lncwwn',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
 
